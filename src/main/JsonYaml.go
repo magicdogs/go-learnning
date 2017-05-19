131e6877c4779198734f9d8978b1ae10b70cd348
magicdog <80099856@qq.com>
2017-05-19 18:31:22 +0800

    type def struct

---
+++ src/main/JsonYaml.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+
+type Student struct {
+	Name    string
+	Age     int
+	Guake   bool
+	Classes []string
+	Price   float32
+}
+
+
+func main() {
+
+	st := &Student {
+		"Xiao Ming",
+		16,
+		true,
+		[]string{"Math", "English", "Chinese"},
+		9.99,
+	}
+
+	b,_ := json.Marshal(st)
+
+	js := string(b)
+	fmt.Println(js)
+
+	ft := &Student{}
+	json.Unmarshal([]byte(js),&ft)
+	fmt.Println(ft.Classes)
+
+
+}
